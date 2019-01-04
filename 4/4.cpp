8d640fe020f3b0f2225db9fcd7650d87e75293f6
Makik <j2152662@126.com>
2019-01-05 00:45:29 +0800

    4 Need to Test

---
+++ 4/4.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <cmath>
+#define INF 1000000000
+using namespace std;
+vector<int> fm[4];
+double Center[4][5]; // Center
+double NodeSet[160][5];
+int BelongTo[160];
+double GetInstance(int x,int y){
+    double Sum = 0;
+    for (int i = 1; i <= 4;i ++)
+        Sum += pow(NodeSet[x][i] - Center[y][i], 2);
+    return sqrt(Sum);
+}
+
+int GetBelongTo(double Result[4]){
+    double temp = INF;
+    double No;
+    for (int i = 1; i <= 3;i ++){
+        if(temp > Result[i]){
+            temp = Result[i];
+            No = i;
+        }
+    }
+    return No;
+}
+
+void Divide(){
+    for (int i = 1; i <= 150;i ++){
+        double Result[4];
+        for (int j = 1; j <= 3;j ++)
+            Result[j] = GetInstance(i, j);
+        BelongTo[i] = GetBelongTo(Result);
+        fm[BelongTo[i]].push_back(i);
+    }
+}
+void init(){
+    ofstream FILE;
+    FILE.open("Iris.txt", ios::in);
+    if(FILE.bad()){
+        cout << "文件打开失败";
+        exit(0);
+    }
+    FILE.close();
+    for (int i = 1; i <= 150;i ++)
+        for (int j = 1; j <= 4;j ++)
+            FILE >> NodeSet[i][j];
+    for (int i = 1; i <= 3;i ++)
+        for (int j = 1; j <= 4;j ++)
+            Center[i][j] = NodeSet[i][j];
+}
+
+void work(){
+    do{
+        bool flag = 0;
+        Divide();//分类
+        for (int i = 1; i <= 3;i ++){
+            for (int j = 1; j <= 4;j ++){
+                double t = 0;
+                for (int k = 0; k < fm[i].size();i ++){
+                    t += NodeSet[fm[i][k]][j];
+                }
+                t /= (double) fm[i].size();
+                if(t != Center[i][j])
+                    flag = 1;
+                Center[i][j] = t;
+            }
+        }
+    } while (flag);
+}
+int main()
+{
+    init();
+    work();
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 This is the set of NUAA datasture homework of 1617001.
+1: Easy but complicated Link Node;
+2: Use stack to calculate segment
+3: Basic string operate && classic dfs problem.
 The last one is optional and from the HuaWei programing contest problems.
