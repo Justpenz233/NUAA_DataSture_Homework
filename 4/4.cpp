5b3a1c49b531ee44871b74cd6bc620aee328ef64
Makik <j2152662@126.com>
2019-01-05 15:24:39 +0800

    Finsh 1234

--- 4/4.cpp
+++ 4/4.cpp
@@ -8,6 +8,17 @@ vector<int> fm[4];
 double Center[4][5]; // Center
 double NodeSet[160][5];
 int BelongTo[160];
+
+void debug(){
+    for (int i = 1; i <= 3;i ++){
+        printf("%d: ", i);
+        for (int j = 1; j <= 4;j ++){
+            printf("%lf    ", Center[i][j]);
+        }
+        printf("%d\n");
+    }
+}
+
 double GetInstance(int x,int y){
     double Sum = 0;
     for (int i = 1; i <= 4;i ++)
@@ -19,7 +30,7 @@ int GetBelongTo(double Result[4]){
     double temp = INF;
     double No;
     for (int i = 1; i <= 3;i ++){
-        if(temp < Result[i]){
+        if(temp > Result[i]){
             temp = Result[i];
             No = i;
         }
@@ -37,38 +48,42 @@ void Divide(){
     }
 }
 void init(){
-    ofstream FILE;
-    FILE.open("Iris.txt", ios::in);
-    if(FILE.bad()){
+    ifstream FILE;
+    FILE.open("F:\\数据结构课设\\4\\Iris.txt");
+    if(!FILE.good()){
         cout << "文件打开失败";
         exit(0);
     }
-    FILE.close();
     for (int i = 1; i <= 150;i ++)
         for (int j = 1; j <= 4;j ++)
-            FILE >> NodeSet[i][j];
+                FILE >> NodeSet[i][j];
     for (int i = 1; i <= 3;i ++)
         for (int j = 1; j <= 4;j ++)
             Center[i][j] = NodeSet[i][j];
+    FILE.close();
 }
+
+
 
 int work(){
     int count = 0;
+    bool flag = 0;
     do{
-        bool flag = 0;
+        flag = 0;
         Divide();//分类
         for (int i = 1; i <= 3;i ++){
             for (int j = 1; j <= 4;j ++){
                 double t = 0;
-                for (int k = 0; k < fm[i].size();i ++){
+                for (int k = 0; k < fm[i].size();k ++)
                     t += NodeSet[fm[i][k]][j];
-                }
                 t /= (double) fm[i].size();
                 if(t != Center[i][j])
                     flag = 1;
                 Center[i][j] = t;
             }
         }
+        if(flag)
+            return ++count;
         for (int i = 1; i <= 3;i ++)
             fm[i].clear();
         count++;
@@ -78,7 +93,7 @@ int work(){
 void showAns(int count){
     printf("共迭代%d次\n", count);
     for (int i = 1; i <= 3;i ++){
-        printf("第%d类: \n");
+        printf("第%d类: \n", i);
         for (int j = 0; j < fm[i].size();j ++)
             printf("%d ", fm[i][j]);
         printf("\n");
@@ -88,4 +103,5 @@ int main()
 {
     init();
     showAns(work());
+    system("pause");
 }
