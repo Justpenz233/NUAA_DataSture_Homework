e114166be802e157d9c52d0ca7b2a87717f1f1d3
Makik <j2152662@126.com>
2019-01-08 20:46:50 +0800

    Finish 9

--- 9/9.cpp
+++ 9/9.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
+#include <cmath>
 using namespace std;
 struct TEL
 {
@@ -8,9 +9,10 @@ struct TEL
    char email[50];
    char name[20];
    char sex[10];
+   int count = 0;
 
    TEL(string s){
-      memcpy(phone, s.c_str(),szieof(phone));
+      memcpy(phone, s.c_str(),sizeof(phone));
    }
    TEL(){};
 
@@ -19,7 +21,7 @@ struct TEL
       INPUT >> name >> sex >> phone >> email;
    }
    void show(){
-      printf("%s %s %s %s\n", name, sex, phone, email);
+      printf("%s %s %s %s 散列次数:%d\n\n", name, sex, phone, email, count);
    }
 
    unsigned int Hash1(){
@@ -28,37 +30,44 @@ struct TEL
          hash = hash * 31 + phone[i];
       }
       hash %= 43;
-      return abs(hash);
+      return abs((int)hash);
    }
    unsigned int Hash2(){
       unsigned int hash = 0;
       for (int i = 0; i < strlen(phone); i++)
          hash = hash * 10 + (phone[i] - '0');
       hash %= 43;
-      return abs(hash);
+      return abs((int)hash);
    }
 
-   bool operator ==(const TEL &t){
-      return memcmp(phone, t.phone, sizeof(phone)) == 0;
+   bool equal(TEL t){
+      if(strlen(t.phone) != strlen(phone))
+         return 0;
+      for (int i = 0; i < strlen(phone);i ++)
+         if(t.phone[i] != phone[i])
+            return 0;
+      return 1;
    }
 };
 
 TEL* HashTable[43];
-int len[43];
 
 int serch(TEL t){
    int key = t.Hash1();
    int step = t.Hash2();
    int hash = 0;
-   while(HashTable[key] != NULL && ++hash < 43){
-      key += step;
-      if(*HashTable[key] == t){
-         len[key] = has;
+   while (HashTable[key] != NULL && (++hash) < 43){
+      if (HashTable[key]->equal(t)) {
+         HashTable[key]->count = hash;
          return key;
       }
+      key += step;
+      key %= 43;
    }
    if(hash == 43)
       return -1;
+   else
+      return key;
 }
 
 int serch(string s){
@@ -66,46 +75,89 @@ int serch(string s){
    int key = t.Hash1();
    int step = t.Hash2();
    int hash = 0;
-   while(HashTable[key] != NULL && ++hash < 43){
-      key += step;
-      if(*HashTable[key] == t){
-         len[key] = hash;
+   while (HashTable[key] != NULL && (++hash) < 43) {
+      if(HashTable[key]->equal(t)){
+         HashTable[key]->count = hash;
          return key;
       }
+      key += step;
+      key %= 43;
    }
    if(hash == 43)
       return -1;
+   else
+      return key;
 }
 
 bool insert(TEL *t)
 {
    int k = serch(*t);
-   if(k != -1)
+   if(k == -1)
       return 0;
-   else
+   else{
       HashTable[k] = t;
+      serch(*t);
+      return 1;
+   }
 }
 
 void init(){
+   for (int i = 0; i < 43;i ++)
+      HashTable[i] = NULL;
+   string FILEPATH;
+   printf("请输入数据文件路径：");
+   cin >> FILEPATH;
    ifstream FILE;
-   //string s;
-   //cin >> s;
-   FILE.open("C:\\Users\\Justpenz233\\Desktop\\DataStrure\\9\\data.txt");
+   FILE.open(FILEPATH, ios::in);
    if(!FILE.good()){
       printf("Open FILE Failed");
       exit(0);
    }
+   else
+      printf("文件打开成功。\n数据导入成功");
+   
+   int debug = 0;
    while(!FILE.eof()){
+      debug++;
       TEL *t = new TEL;
       t->getCin(FILE);
       insert(t);
    }
 }
 
+void GetASL(){
+   double NodeNum = 0;
+   double HashTime = 0;
+   for (int i = 0; i < 43;i ++){
+      if(HashTable[i]){
+         NodeNum++;
+         HashTime += HashTable[i]->count;
+      }
+   }
+   double t = HashTime / NodeNum;
+   printf("平均查找长度为: %lf\n", t);
+}
+
 int main()
 {
    init();
-   for (int i = 0; i < 43;i ++)
-      HashTable[i]->show();
+   GetASL();
+   string s = "";
+   while(s != "#"){
+      printf("请输入您要查找的号码:(输入#结束程序): ");
+      cin >> s;
+      if(s == "#")
+         return 0;
+      if(s.length() != 11){
+         printf("请输入正确的电话号码\n\n");
+         continue;
+      }
+      int k = serch(s);
+      if(k == -1 || HashTable[k] == NULL){
+         printf("您要查找的号码不存在\n\n");
+      }
+      else
+         HashTable[k]->show();
+   }
    system("pause");
 }
